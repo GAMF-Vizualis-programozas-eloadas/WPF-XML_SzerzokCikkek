c53f8d5988a0316318fb24a3427cc92983bde0be
jzscsaba <johanyak.csaba@nje.hu>
2024-10-24 13:15:30 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Szerzok_Cikkek;
+
+namespace WPF_XML_SzerzokCikkek
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private dsAdatok dsAdatok;
+		public MainWindow()
+		{
+			InitializeComponent();
+			dsAdatok = new dsAdatok();
+			Application.Current.Properties["dsAdatok"] = dsAdatok;
+		}
+
+		private void miBetöltés_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				dsAdatok.ReadXml("adatok.xml", XmlReadMode.Auto);
+			}
+			catch (Exception)
+			{
+				var sz1 = dsAdatok.dtSzerzők.AdddtSzerzőkRow(1, "Kocsis Zoltán Tamás");
+				var sz2 = dsAdatok.dtSzerzők.AdddtSzerzőkRow(2, "Kovács János");
+				var sz3 = dsAdatok.dtSzerzők.AdddtSzerzőkRow(3, "Tóth László");
+				var c1 = dsAdatok.dtCikkek.AdddtCikkekRow(1, "Informatikai kutatás a gerincsebészetben",
+					"http://gradus.kefo.hu/current/2020-2/2020_2_CSC_002_Kocsis.pdf");
+				var c2 = dsAdatok.dtCikkek.AdddtCikkekRow(2, "Kéztörésből felépülő páciens erőnlétének vizsgálata mems szenzor segítségével",
+					"http://gradus.kefo.hu/archive/2020-1/2020_1_ENG_001_Toth.pdf");
+				var c3 = dsAdatok.dtCikkek.AdddtCikkekRow(3, "A teniszben használt mozdulatok mozgáselemzése mems szenzorok segítségével neuro-motorikus betegségben szenvedő pácienseknél",
+					"http://gradus.kefo.hu/archive/2019-1/2019_1_CSC_004_Toth.pdf");
+				dsAdatok.dtSzerzőkCikkek.AdddtSzerzőkCikkekRow(sz1, c1);
+				dsAdatok.dtSzerzőkCikkek.AdddtSzerzőkCikkekRow(sz2, c1);
+				dsAdatok.dtSzerzőkCikkek.AdddtSzerzőkCikkekRow(sz3, c2);
+				dsAdatok.dtSzerzőkCikkek.AdddtSzerzőkCikkekRow(sz3, c3);
+			}
+		}
+
+		private void miMentés_Click(object sender, RoutedEventArgs e)
+		{
+			dsAdatok.WriteXml("adatok.xml", XmlWriteMode.WriteSchema);
+		}
+
+		private void miKilépés_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void miSzerzők_Click(object sender, RoutedEventArgs e)
+		{
+			dgCikkek.Visibility = Visibility.Collapsed;
+			ucKombinalt.Visibility = Visibility.Collapsed;
+			dgSzerzők.Visibility = Visibility.Visible;
+			dgSzerzők.ItemsSource = dsAdatok.dtSzerzők;
+		}
+
+		private void miCikkek_Click(object sender, RoutedEventArgs e)
+		{
+			dgSzerzők.Visibility = Visibility.Collapsed;
+			ucKombinalt.Visibility = Visibility.Collapsed;
+			dgCikkek.Visibility = Visibility.Visible;
+			dgCikkek.ItemsSource = dsAdatok.dtCikkek;
+		}
+
+		private void miKombinált_Click(object sender, RoutedEventArgs e)
+		{
+			dgSzerzők.Visibility = Visibility.Collapsed;
+			dgCikkek.Visibility = Visibility.Collapsed;
+			ucKombinalt.Visibility = Visibility.Visible;
+		}
+	}
+}
---
+++ ucKombinalt.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Szerzok_Cikkek;
+
+namespace WPF_XML_SzerzokCikkek
+{
+	/// <summary>
+	/// Interaction logic for ucKombinalt.xaml
+	/// </summary>
+	public partial class ucKombinalt : UserControl
+	{
+		private dsAdatok dsAdatok;
+		public ucKombinalt()
+		{
+			InitializeComponent();
+		}
+
+		private void cbSzerzők_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var Szerző = (cbSzerzők.SelectedItem as DataRowView)?.Row as
+				dsAdatok.dtSzerzőkRow;
+			var Cikkek = from x in dsAdatok.dtSzerzőkCikkek
+									 where x.dtSzerzőkRow == Szerző
+									 select
+									 new
+									 {
+										 Szerzők = (from y in dsAdatok.dtSzerzőkCikkek
+																where y.dtCikkekRow == x.dtCikkekRow
+																select
+																	y.dtSzerzőkRow.Név).Aggregate("",
+													 (a, c) => a + c + ", "),
+										 x.dtCikkekRow.Cím,
+										 x.dtCikkekRow.URL
+									 };
+
+			tbCikkek.Text = Cikkek.Aggregate("", (a, c) => a + c.Szerzők + c.Cím + ", " + c.URL + "\r\n\r\n");
+		}
+
+		private void ucKombinalt_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			if (!IsVisible) return;
+			dsAdatok = Application.Current.Properties["dsAdatok"] as dsAdatok;
+			cbSzerzők.ItemsSource = dsAdatok?.dtSzerzők;
+			cbSzerzők.DisplayMemberPath = "Név";
+			cbSzerzők.SelectedIndex = 0;
+		}
+	}
+}
